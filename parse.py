fe65624e8f4ea59dd0b7808c64f8b745f77f1d37
Carl Drougge <bearded@longhaired.org>
2012-11-17 20:23:54 +0100

    Clean up (still does the same thing).

--- parse.py
+++ parse.py
@@ -4,57 +4,44 @@
 from wellpapp.util import TIFF
 from sys import argv
 
-fh = open(argv[1], "rb")
-t = TIFF(fh)
-ifd = t.subifd[0]
-width, height = t.ifdget(ifd, 256)[0], t.ifdget(ifd, 257)[0]
-bitspersample = t.ifdget(ifd, 258)[0]
-assert t.ifdget(ifd, 277)[0] # SamplesPerPixel
-z = t.ifdget(ifd, 279)[0]
-off = t.ifdget(ifd, 273)[0]
+class DNG:
+	def __init__(self, fh):
+		t = TIFF(fh)
+		ifd = t.subifd[0]
+		self.width, self.height = t.ifdget(ifd, 256)[0], t.ifdget(ifd, 257)[0]
+		self.bitspersample = t.ifdget(ifd, 258)[0]
+		assert t.ifdget(ifd, 277)[0] # SamplesPerPixel
+		self.raw_size = t.ifdget(ifd, 279)[0]
+		self.offset = t.ifdget(ifd, 273)[0]
+		fh.seek(self.offset)
+		self.data = fh.read(self.raw_size)
+		assert len(self.data) == self.raw_size
+		self._pos = 0
+		self._have_bits = 0
+		self._val = 0
+	
+	def get_pixel(self):
+		byte = ord(self.data[self._pos])
+		self._pos += 1
+		if self._have_bits + 8 > self.bitspersample:
+			want = self.bitspersample - self._have_bits
+			mask = (1 << want) - 1
+			self._have_bits = 8 - want
+			res = self._val << want | (byte & ~mask) >> self._have_bits
+			self._val = byte & mask
+			return res
+		else:
+			self._val = self._val << 8 | byte
+			self._have_bits += 8
+			if self._have_bits == self.bitspersample:
+				res = self._val
+				self._have_bits = self._val = 0
+				return res
+			else:
+				return self.get_pixel()
 
-data = """P5
-4000 3000
-255
-"""
+raw = DNG(open(argv[1], "rb"))
 ofh = open("out.pgm", "wb")
-ofh.write(data)
-
-fh.seek(off)
-data = fh.read(z)
-
-have_bits = 0
-val = 0
-out = []
-row = []
-imax = 0
-values = []
-
-def put(v):
-	global row, imax
-	row.append(v)
-	if len(row) == width:
-		row = row[:4000]
-		m = max(row)
-		print len(out), m
-		imax = max(imax, m)
-		out.append("".join(chr(v >> 4) for v in row))
-		values.extend(row)
-		row = []
-
-for byte in map(ord, data):
-	if have_bits + 8 > bitspersample:
-		want = bitspersample - have_bits
-		mask = (1 << want) - 1
-		have_bits = 8 - want
-		put(val << want | (byte & ~mask) >> have_bits)
-		val = byte & mask
-	else:
-		val = val << 8 | byte
-		have_bits += 8
-		if have_bits == bitspersample:
-			put(val)
-			have_bits = val = 0
-
-print imax
-ofh.write("".join(out))
+ofh.write("P5\n4000 3000\n255\n")
+for r in range(raw.height):
+	ofh.write("".join(chr(raw.get_pixel() >> 4) for p in range(raw.width))[:4000])
